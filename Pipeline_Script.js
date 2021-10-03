80cd2bd0f80c7fa529e597629cc9bbc36d077c97
bandrews9832 <91579953+bandrews9832@users.noreply.github.com>
2021-10-03 13:54:46 -0400

    Update Pipeline_Script.js
    Cleanup pipeline script

--- Pipeline_Script.js
+++ Pipeline_Script.js
@@ -4,15 +4,13 @@ node {
             bat 'rmdir /S /Q heroes-react'
             bat 'git clone https://github.com/bandrews9832/heroes-react.git'
         }
-               //git branch: 'Tech Task Wk3 Pipeline', url: 'https://github.com/bandrews9832/heroes-react.git'
     }
     stage('Build') {
         //update "heroes-react" to your project name
         dir('D:\\Documents\\Grad School\\Large Scale Application Deployment\\Jenkins Directory\\heroes-react') {
             bat 'npm install' //install dependencies
-            //bat 'npm audit fix' //correct any potential vulnerabilities 
             bat 'npm run build -- --prod' //run build
-            archiveArtofacts 'build/**'
+            //archiveArtofacts 'build/**'
         }
     }
     stage('Deploy') {
